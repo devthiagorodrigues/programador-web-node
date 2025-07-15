32cbc91bf856287c8f84498df5439bc90f4ab8be
Your Name <you@example.com>
2025-07-15 19:54:20 -0300

    Alfala Books

--- alfala_books/src/App.js
+++ alfala_books/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Logo from './components/Logo';
-
+import LogoHeader from './components/LogoHeader';
+import OptionsHeader from './components/OptionsHeader';
+import IconesHeader from './components/IconesHeader';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-        <Logo />
+        <LogoHeader />
+        <OptionsHeader />
+        <IconesHeader />
+
+        
       </header>
     </div>
   );
--- alfala_books/src/components/LogoHeader/index.js
+++ alfala_books/src/components/LogoHeader/index.js
@@ -1,4 +1,5 @@
 import logo from '../../img/logo.svg'
+import './style.css'
 
 function Logo () {
     return (
