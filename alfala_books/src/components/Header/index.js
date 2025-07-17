cd482b7dd61506c60773eb97c623ca2fb8521fcf
Your Name <you@example.com>
2025-07-16 22:14:48 -0300

    Refactor App component and update styles
    - Replaced Logo component with LogoHeader, OptionsHeader, and IconesHeader in App.js.
    - Updated App.css to include a gradient background and adjusted header styles.
    - Removed unused Logo component and its associated styles.

--- alfala_books/src/components/Header/index.js
+++ alfala_books/src/components/Header/index.js
@@ -1,14 +1,25 @@
-import './style.css'
 import LogoHeader from '../LogoHeader';
 import OptionsHeader from '../OptionsHeader';
 import IconesHeader from '../IconesHeader';
 
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
+
 function Header() {
     return (
         <header className="App-header">
-            <LogoHeader />
-            <OptionsHeader />
-            <IconesHeader />
+
+            <HeaderContainer>
+                <LogoHeader />
+                <OptionsHeader />
+                <IconesHeader />
+            </HeaderContainer>
+
         </header>
     )
 }
